00d2e748e08fb4e8bcc34398f90c837514250381
Kevin Allen <kevin.allen91@gmail.com>
2019-09-20 14:04:26 -0700

    fixed errors

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,7 +3,7 @@ import axios from "axios"
 import CharacterCard from "./CharacterCard"
 // import {Link} from "react-router-dom"
 
-export default function CharacterList(props) {
+const CharacterList = (props) => {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacter] = useState([]);
 
@@ -15,8 +15,8 @@ export default function CharacterList(props) {
     .get(`https://rickandmortyapi.com/api/character/`)
     .then(response => {
       console.log(response);
-      var results = response.data.results;
-      setCharacter(results);
+      
+      setCharacter(response.data.results);
     })
     .catch(err => {console.log("ERROR", err)})    
     
@@ -40,12 +40,33 @@ export default function CharacterList(props) {
     </section>
   );
 
-//   function CharacterDetails({characters}) {
+
+}
+
+// const CharacterSearch = ({characters}) => {
+//     const [searchTerm, setSearchTerm] = useState('');
+//   const [searchResults, setSearchResults] = useState([]);
+
+//   useEffect (() => {
+//     const results = characters.filter(character => {
+//       character.toLowerCase().includes(searchTerm)
+//     });
+//     setSearchResults(results)
+//   })
 //   return (
-//     <Link to={`/characters/${characters.id}`} >
-//     <CharacterCard {...characters} />
+//     <form>
+//       <label for="search">Search:</label>
+//       <input 
+//         id="search"
+//         type="text"
+//         name="textfield"
+//         value={searchTerm}
+//         onChange={handleChange}
+//         />
+//       </form>
     
-//     </Link>
+    
 //   );
 // }
-}
+export default CharacterList;
+
